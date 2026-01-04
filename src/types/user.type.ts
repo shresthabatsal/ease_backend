80ea491b32af389769ca8662291e7172beb3b23c
batsal <shresthabatsal247@gmail.com>
2026-01-04 22:44:22 +0545

    add user zod schema

---
+++ src/types/user.type.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const UserSchema = z.object({
+  fullName: z.string().min(1, "Full name is required."),
+  email: z.email("Invalid email address."),
+  phone: z.string().optional(),
+  password: z.string().min(6, "Password must be at least 6 characters."),
+  role: z.enum(["USER", "ADMIN"]).default("USER"),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
