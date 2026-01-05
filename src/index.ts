795533605ce1c1731df634593cdb88d24b46a1f0
batsal <shresthabatsal247@gmail.com>
2026-01-05 08:53:48 +0545

    add auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
 import { PORT } from "./config";
 import { connectDatabase } from "./database/mongodb";
+import router from "./routes/auth.route";
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+  return res.status(200).json({ success: "true", message: "Welcome." });
+});
+app.use("/api/auth", router);
 
 async function startServer() {
   await connectDatabase();
