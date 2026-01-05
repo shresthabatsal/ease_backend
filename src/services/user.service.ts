96b063d9187a83c5f5cca40b9a1cac3ea3ed218a
batsal <shresthabatsal247@gmail.com>
2026-01-05 08:36:58 +0545

    add user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,65 @@
+import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import { UserRepository } from "../repositories/user.repository";
+import bcryptjs from "bcryptjs";
+import { HttpError } from "../errors/http.error";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+
+const userRepository = new UserRepository();
+
+export class UserService {
+
+  async createUser(data: CreateUserDTO) {
+    // Check if email already exists
+    const existingUser = await userRepository.getUserByEmail(data.email);
+    if (existingUser) {
+      throw new HttpError(409, "Email already in use.");
+    }
+
+    // Hash password
+    const hashedPassword = await bcryptjs.hash(data.password, 10);
+
+    // Create user
+    const newUser = await userRepository.createUser({
+      fullName: data.fullName,
+      email: data.email,
+      phoneNumber: data.phoneNumber,
+      password: hashedPassword,
+      role: data.role,
+    });
+
+    return newUser;
+  }
+
+  async loginUser(data: LoginUserDTO) {
+    const user = await userRepository.getUserByEmail(data.email);
+    if (!user) {
+      throw new HttpError(404, "User not found.");
+    }
+
+    // Compare password
+    const isValidPassword = await bcryptjs.compare(
+      data.password,
+      user.password
+    );
+
+    if (!isValidPassword) {
+      throw new HttpError(401, "Invalid credentials.");
+    }
+
+    // Generate JWT
+    const payload = {
+      userId: user._id.toString(),
+      role: user.role,
+    };
+
+    const token = jwt.sign(payload, JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    return {
+      token,
+      user,
+    };
+  }
+}
