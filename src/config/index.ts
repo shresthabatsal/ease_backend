67b31669954bd99686a79a1e6a00d8b9bffa9aaa
batsal <shresthabatsal247@gmail.com>
2026-01-05 08:28:00 +0545

    add http error class and jwt config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Server Port
-export const PORT = process.env.PORT
-  ? parseInt(process.env.PORT)
-  : 3000;
+export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // MongoDB Connection String
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/default_db";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/default_db";
+
+// JWT Secret
+export const JWT_SECRET: string = process.env.JWT_SECRET || "default";
