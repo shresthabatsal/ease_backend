93ce929089d94b9d8cf17624a3abf85fc6247791
batsal <shresthabatsal247@gmail.com>
2026-01-19 23:29:20 +0545

    add cors config

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -7,16 +7,7 @@ export const CreateUserDTO = UserSchema.pick({
   phoneNumber: true,
   password: true,
   role: true
-}).extend({
-  confirmPassword: z.string().min(6, "Confirm your password.")
-}).refine(
-  (data) => data.password === data.confirmPassword,
-  {
-    message: "Passwords do not match.",
-    path: ["confirmPassword"]
-  }
-);
-
+})
 export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
 
 export const LoginUserDTO = z.object({
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
 import express from "express";
+import cors from "cors";
 import { PORT } from "./config";
 import { connectDatabase } from "./database/mongodb";
 import router from "./routes/auth.route";
 
 const app = express();
 
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3003",
+    "http://localhost:3005",
+  ],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
-app.get('/test', (req, res) => {
+app.get("/test", (req, res) => {
   return res.status(200).json({ success: "true", message: "Welcome." });
 });
+
 app.use("/api/auth", router);
 
 async function startServer() {
